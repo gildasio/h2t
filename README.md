cfe3e1a7d605a05937c5d36f695cfff8ecbab577
Gildásio Júnior <gildasio@protonmail.com>
2019-02-26 20:08:48 -0300

    Update documentation

--- README.md
+++ README.md
@@ -1,15 +1,64 @@
-# H2T - HTTP Hardening Tool
+# h2t - HTTP Hardening Tool
 
 ## Description
 
+**h2t** is a simple tool to help sysadmins to hardening their websites.
+
+Until now **h2t** checks the website headers and recommends how to make it better.
+
+## Dependences
+
+* [Python 3](https://www.python.org/download/releases/3.0/)
+* [colorama](https://github.com/tartley/colorama)
+* [requests](http://docs.python-requests.org/en/master/)
+
 ## Install
 
-### Dependences
-
-### Tested on
+~~~
+$ git clone https://github.com/gildasio/h2t
+$ cd h2t
+$ pip install -r requirements.txt
+$ ./h2t.py -h
+~~~
 
 ## Usage
 
-### Help Menu
+~~~
+$ ./h2t.py -h
+      eeee
+e   e    8 eeeee
+8   8    8   8
+8eee8 eee8   8e
+88  8 8      88
+88  8 8eee   88
 
-### Examples
+https://github.com/gildasio/h2t
+
+usage: h2t.py [-h] [-v] [-a] [-g] [-b] [-n] [-r | -s] url
+
+h2t - HTTP Hardening Tool
+
+positional arguments:
+  url                   url to look for
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         increase output verbosity
+  -a, --all             look at all cataloged headers
+  -g, --good            look at good headers only
+  -b, --bad             look at bad headers only
+  -n, --no-redirect     doesn't follow http redirects
+  -r, --recommendation  output only recommendations
+  -s, --status          output recommendations and actual status (eg: existent
+                        headers)
+~~~
+
+### Screenshots
+
+#### Help
+
+![h2t help menu](docs/help.png)
+
+#### My website
+
+![h2t against my website](docs/gildasio.png)
--- h2t.py
+++ h2t.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 
 from src import output
@@ -5,21 +7,6 @@ from src import banners
 from src import connection
 from src import files
 from src import core
-
-parser = argparse.ArgumentParser(description="h2t - HTTP Hardening Tool")
-parser.add_argument("-v", "--verbose", action="count", default=0, help="increase output verbosity")
-parser.add_argument("-a", "--all", action="store_true", default=True, help="look at all cataloged headers")
-parser.add_argument("-g", "--good", action="store_true", help="look at good headers only")
-parser.add_argument("-b", "--bad", action="store_true", help="look at bad headers only")
-parser.add_argument("-n", "--no-redirect", action="store_false", help="doesn't follow http redirects")
-
-groupOutput = parser.add_mutually_exclusive_group()
-groupOutput.add_argument("-r", "--recommendation", action="store_true", default=True, help="output only recommendations")
-groupOutput.add_argument("-s", "--status", action="store_true", help="output recommendations and actual status (eg: existent headers)")
-
-parser.add_argument("url", help="url to look for")
-
-args = parser.parse_args()
 
 def check(response, category):
 	if category == "good":
@@ -33,6 +20,21 @@ def check(response, category):
 
 if __name__ == '__main__':
 	output.banner(banners.getBanner())
+
+	parser = argparse.ArgumentParser(description="h2t - HTTP Hardening Tool")
+	parser.add_argument("-v", "--verbose", action="count", default=0, help="increase output verbosity")
+	parser.add_argument("-a", "--all", action="store_true", default=True, help="look at all cataloged headers")
+	parser.add_argument("-g", "--good", action="store_true", help="look at good headers only")
+	parser.add_argument("-b", "--bad", action="store_true", help="look at bad headers only")
+	parser.add_argument("-n", "--no-redirect", action="store_false", help="doesn't follow http redirects")
+
+	groupOutput = parser.add_mutually_exclusive_group()
+	groupOutput.add_argument("-r", "--recommendation", action="store_true", default=True, help="output only recommendations")
+	groupOutput.add_argument("-s", "--status", action="store_true", help="output recommendations and actual status (eg: existent headers)")
+
+	parser.add_argument("url", help="url to look for")
+
+	args = parser.parse_args()
 
 	if "://" not in args.url:
 		args.url = "http://" + args.url
